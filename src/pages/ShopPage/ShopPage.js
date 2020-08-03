acb1166bdd73b9117e08f1ce3f2a6f11a0dab4a1
unknown <diegollanespasos@hotmail.com>
2020-08-02 22:08:53 -0500

    add ed to firestore the Shop Data and created the action to update the collections with the data from firestore

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview';
 import { Route } from 'react-router-dom';
 import CollectionPage from '../CollectionPage/CollectionPage';
+import { firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
+import {connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop-actions';
 
-const ShopPage = ({ match }) => {
-    return(
-        <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />           
-        </div> 
-    )
-}
+class ShopPage extends Component {
 
-export default ShopPage;
+    unsuscribedFromSnapShot = null;
+
+    componentDidMount(){
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection('collections');
+
+        collectionRef.onSnapshot( async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+        })
+    }
+
+    render() {
+        const { match } = this.props   
+        return(
+            <div className='shop-page'>
+                <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />           
+            </div> 
+        )
+    }
+};
+
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage);
